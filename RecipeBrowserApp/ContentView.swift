877f7c99d77ba59c35f3ca6498e5ab6c801d8068
Abdul Rehman <aali@circlonline.com>
2023-10-27 15:02:43 +0500

    Initial Commit

---
+++ RecipeBrowserApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeBrowserApp
+//
+//  Created by Abdul RehmanAli on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeBrowserApp/RecipeBrowserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserAppApp.swift
+//  RecipeBrowserApp
+//
+//  Created by Abdul RehmanAli on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
